b8f298a1cba319c444b447f7d1aaa2bd5d6ed88b
Cynthia <lazokmarios7@gmail.com>
2024-08-04 11:05:05 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\AitoolsController;
+use App\Http\Controllers\BlogPostsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::resource('categories', CategoriesController::class);
 Route::resource('aitools', AitoolsController::class);
+Route::resource('blogposts', BlogPostsController::class);
